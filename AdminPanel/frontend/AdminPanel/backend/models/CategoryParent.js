608182390d973d021fca0478e45d9b4a1485cec6
Garvit <garvitkumar504@gmail.com>
2025-05-23 22:55:32 +0530

    Second Commit

--- AdminPanel/frontend/AdminPanel/backend/models/CategoryParent.js
+++
@@ -1,9 +0,0 @@
-// models/CategoryParent.js
-import mongoose from "mongoose";
-const categoryParentSchema = new mongoose.Schema({
-  name: { type: String, required: true }, // festival, event, business
-  slug: { type: String, required: true, unique: true },
-  status: { type: String, enum: ['active', 'inactive'], default: 'active' },
-});
-
-export default mongoose.models.CategoryParent || mongoose.model('CategoryParent', categoryParentSchema);
